8675d1df5108d6c1907bffcdc28bb812ccb35ff4
kyrhaTV <kyrha1719@gmail.com>
2018-10-06 16:41:57 -0400

    Added few SFX

--- WonderJam2018/Assets/Scripts/Laser.cs
+++ WonderJam2018/Assets/Scripts/Laser.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Laser : MonoBehaviour
 {
     private IEnumerator coroutine;
-
+    AudioSource LaserSound;
     // Use this for initialization
     void Start()
     {
@@ -21,9 +21,16 @@ public class Laser : MonoBehaviour
     IEnumerator DisableLaser()
     {
         yield return new WaitForSeconds(2);
+
         GetComponent<SpriteRenderer>().enabled = !GetComponent<SpriteRenderer>().enabled;
+        //PlayAudio();
         GetComponent<BoxCollider2D>().enabled = !GetComponent<BoxCollider2D>().enabled;
         StartCoroutine(DisableLaser());
+    }
+
+    public void PlayAudio() {
+        LaserSound = GetComponent<AudioSource>();
+        LaserSound.Play(0);
     }
 
 
