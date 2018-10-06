d2a1df7b81a6f9c9da592ea7fb8f16af254a70d9
kyrhaTV <kyrha1719@gmail.com>
2018-10-06 04:29:41 -0400

    JuJu + JM

---
+++ WonderJam2018/Assets/Scripts/LoadScreenOnClick.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScreenOnClick : MonoBehaviour {
+
+
+    public void LoadByIndex(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+
+    void setButtonActive()
+    {
+        GameObject go = GameObject.Find("BackButton");
+        go.SetActive(GameObject.Find("BackButton"));
+    }
+}
