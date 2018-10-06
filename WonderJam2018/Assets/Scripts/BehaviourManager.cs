3618f1527e393cf8ccd9f8f4fdf6195be86e9a2c
William Zerbato <william.zerbato@esme.fr>
2018-10-06 12:57:45 -0400

    Jacob + Justine +Jim+ aurelien + Sou + William

---
+++ WonderJam2018/Assets/Scripts/BehaviourManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BehaviourManager : MonoBehaviour {
+
+    public List<GameObject> obstacles;
+    public Slider timerBar;
+	
+	// Update is called once per frame
+	void SwitchBehaviour()
+    {
+        if(timerBar.value == 10 || timerBar.value == 0)
+        {
+            foreach(GameObject items in obstacles)
+            {
+                
+            }
+        }
+    }
+
+}
