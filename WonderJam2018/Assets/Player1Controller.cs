abf1577895cdd879c07a239e6310d4276bd848b8
deosadamas <deosadamaslol@gmail.com>
2018-10-06 14:12:55 -0400

    test perso

--- WonderJam2018/Assets/Player1Controller.cs
+++ WonderJam2018/Assets/Player1Controller.cs
@@ -11,6 +11,19 @@ public class Player1Controller : MonoBehaviour {
 
     Animator Anim;
 
+    bool grounded = false;
+
+    public Transform groudCheck;
+
+    float groudRadius = 0.2f;
+
+    public float jumpForce = 700f;
+
+    public LayerMask whatisGroud;
+
+
+
+
     void Start() {
 
         Anim = GetComponent<Animator>();
@@ -18,6 +31,13 @@ public class Player1Controller : MonoBehaviour {
 
 
     void FixedUpdate() {
+
+
+        grounded = Physics2D.OverlapCircle(groudCheck.position, groudRadius, whatisGroud);
+
+        Anim.SetBool("Ground", grounded);
+
+        Anim.SetFloat("vSpeed", GetComponent<Rigidbody2D>().velocity.y);
 
         float move = Input.GetAxis("Horizontal");
 
@@ -32,6 +52,16 @@ public class Player1Controller : MonoBehaviour {
         }
         else if (move < 0 && facingRight) {
             Flip();
+        }
+    }
+
+    private void Update()
+    {
+        if (grounded && Input.GetKeyDown(KeyCode.Space)) {
+            Anim.SetBool("Ground", false);
+
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce));
+
         }
     }
 
