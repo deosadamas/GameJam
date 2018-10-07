f412a3af3361f1cd232db956844ac61129e0244c
Souhail BEN ALI <benalisouhail2@gmail.com>
2018-10-06 20:00:57 -0400

    Sou

---
+++ WonderJam2018/Assets/Headquarter/png/Objects/non-tileable/broken_box/scripts/DestructableBase.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class DestructableBase : Indestructible {
+	
+	public float destructionImpulse = 10000f;
+	public bool automaticCalculatePeaceMass = true;
+
+	private float mBreakRandomCofficient = 3.0f;
+
+
+	protected virtual void Start()
+	{				
+		if (automaticCalculatePeaceMass) 
+		{
+			foreach (Transform t in transform)
+			{				
+				t.gameObject.SetActive(true);					
+				
+				t.GetComponent<Rigidbody2D>().mass = ( Mathf.Pow( t.GetComponent<Collider2D>().bounds.size.magnitude, 2f )) / ( Mathf.Pow (GetComponent<Collider2D>().bounds.size.magnitude,2f )) * GetComponent<Rigidbody2D>().mass;					
+
+				t.gameObject.SetActive(false);
+			}
+		}
+
+        else
+        {
+            foreach (Transform t in transform)
+            {
+                t.gameObject.SetActive(true);
+
+                t.GetComponent<Rigidbody2D>().mass = (Mathf.Pow(t.GetComponent<Collider2D>().bounds.size.magnitude, 2f)) / (Mathf.Pow(GetComponent<Collider2D>().bounds.size.magnitude, 2f)) * 500;
+
+                t.gameObject.SetActive(false);
+            }
+        }
+	}
+
+
+	protected virtual void OnCollisionEnter2D( Collision2D collision )
+	{
+		Vector2 myImpulse = Impulse;
+
+		Vector2 otherImpulse = Vector2.zero;
+		foreach (ContactPoint2D cp in collision.contacts ) 
+		{
+			IPhysics phys = cp.collider.GetComponent(typeof(IPhysics)) as IPhysics;
+
+			if ( phys != null )
+			{
+				otherImpulse += phys.Impulse;
+			}
+			else if ( cp.collider.GetComponent<Rigidbody2D>() == null )
+			{
+				otherImpulse = Vector3.zero;
+			}
+		}
+
+		Vector2 resultImpulse = myImpulse + otherImpulse;
+
+		float deltaImpulse = Mathf.Abs (myImpulse.magnitude - otherImpulse.magnitude);
+
+		if ( deltaImpulse > destructionImpulse ) 
+		{
+			StartCoroutine( "CheckDestroySelf" );
+		}
+	}
+
+	IEnumerator CheckDestroySelf()
+	{
+		yield return new WaitForFixedUpdate();
+
+		List<Transform> childs = new List<Transform>();
+
+		foreach( Transform t in transform )
+		{
+			childs.Add( t );
+		}
+
+		foreach( Transform t in childs )
+		{
+			t.parent = null;
+			t.gameObject.SetActive( true );
+			t.GetComponent<Rigidbody2D>().velocity = ( ( ( mBreakRandomCofficient * Random.insideUnitCircle + GetComponent<Rigidbody2D>().velocity.normalized ).normalized * GetComponent<Rigidbody2D>().velocity.magnitude ));
+			t.GetComponent<Rigidbody2D>().angularVelocity = GetComponent<Rigidbody2D>().angularVelocity;
+		}
+
+		Destroy( gameObject );
+    }
+}
---
+++ WonderJam2018/Assets/Headquarter/png/Objects/non-tileable/broken_box/scripts/PlayerPlatformerController.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerPlatformerController : PhysicsObject
+{
+
+    public float maxSpeed = 7;
+    public float jumpTakeOffSpeed = 7;
+    public Text scoreText;
+    public GameObject heart1, heart2, heart3, heart4, gameOver;
+
+    //score & healt
+    int score;
+    int health;
+
+    private void Start()
+    {
+        score = 0;
+        health = 4;
+        setScoreText();
+        heart1.gameObject.SetActive(true);
+        heart2.gameObject.SetActive(true);
+        heart3.gameObject.SetActive(true);
+        heart4.gameObject.SetActive(true);
+        gameOver.gameObject.SetActive(false);
+    }
+
+
+
+    private SpriteRenderer spriteRenderer;
+
+    // Use this for initialization
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+
+
+    protected override void ComputeVelocity()
+    {
+        Vector2 move = Vector2.zero;
+
+        move.x = Input.GetAxis("Horizontal");
+
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            velocity.y = jumpTakeOffSpeed;
+        }
+        else if (Input.GetButtonUp("Jump"))
+        {
+            if (velocity.y > 0)
+            {
+                velocity.y = velocity.y * 0.5f;
+            }
+        }
+
+        bool flipSprite = (spriteRenderer.flipX ? (move.x > 0.01f) : (move.x < 0.01f));
+        if (flipSprite)
+        {
+            spriteRenderer.flipX = !spriteRenderer.flipX;
+        }
+
+
+        targetVelocity = move * maxSpeed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("Coin"))
+        {
+            col.gameObject.SetActive(false);
+            score = score + 1;
+            setScoreText();
+            
+        }
+
+        if (col.gameObject.CompareTag("box2"))
+        {
+            Destroy(col.gameObject);
+
+
+        }
+
+        if (col.gameObject.CompareTag("enemy"))
+        {
+
+            health = health - 1;
+
+            switch (health)
+            {
+                case 4:
+                    heart1.gameObject.SetActive(true);
+                    heart2.gameObject.SetActive(true);
+                    heart3.gameObject.SetActive(true);
+                    heart4.gameObject.SetActive(true);
+                    break;
+                case 3:
+                    heart1.gameObject.SetActive(true);
+                    heart2.gameObject.SetActive(true);
+                    heart3.gameObject.SetActive(true);
+                    heart4.gameObject.SetActive(false);
+                    break;
+
+                case 2:
+                    heart1.gameObject.SetActive(true);
+                    heart2.gameObject.SetActive(true);
+                    heart3.gameObject.SetActive(false);
+                    heart4.gameObject.SetActive(false);
+                    break;
+
+                case 1:
+                    heart1.gameObject.SetActive(true);
+                    heart2.gameObject.SetActive(false);
+                    heart3.gameObject.SetActive(false);
+                    heart4.gameObject.SetActive(false);
+                    break;
+
+                case 0:
+                    heart1.gameObject.SetActive(false);
+                    heart2.gameObject.SetActive(false);
+                    heart3.gameObject.SetActive(false);
+                    heart4.gameObject.SetActive(false);
+                    gameOver.gameObject.SetActive(true);
+                    Time.timeScale = 0;
+                    break;
+            }
+
+
+        }
+    }
+
+    void setScoreText()
+    {
+        scoreText.text = "Score: " + score.ToString();
+    }
+
+    
+}
